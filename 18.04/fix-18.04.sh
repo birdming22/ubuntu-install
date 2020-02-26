80194e3c7233452dc9fe95707465e6b6d8b98cbc
birdming22 <birdmin22@hotmail.com>
2020-02-26 15:25:36 +0800

    refactor

--- 18.04/fix-18.04.sh
+++ 18.04/fix-18.04.sh
@@ -1,2 +1,11 @@
+# for auto update
 cp 20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+# for ls
 cp dircolors ~/.dircolors
+# timezone
+timedatectl set-timezone Asia/Taipei
+# for tig
+cp tigrc ~/.tigrc
+# disable screen saver
+sed -i 's/mode\:\t\trandom/mode\:\t\toff/g' ~/.xscreensaver
+

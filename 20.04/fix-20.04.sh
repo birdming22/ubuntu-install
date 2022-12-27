51c17f9db2e465e119f9e2c6695b384ccf921a29
Birdming Lai <birdming22@hotmail.com>
2022-12-27 09:18:26 +0800

    add fix-20.04.sh

---
+++ 20.04/fix-20.04.sh
@@ -0,0 +1,20 @@
+# for auto update
+cp 20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
+
+# for ls
+cp dircolors ~/.dircolors
+
+# timezone
+timedatectl set-timezone Asia/Taipei
+
+# for tig
+cp tigrc ~/.tigrc
+
+# disable screen saver
+sed -i 's/mode\:\t\trandom/mode\:\t\toff/g' ~/.xscreensaver
+gsettings set org.gnome.desktop.screensaver lock-delay 3600
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
+
+# disable suspend
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target

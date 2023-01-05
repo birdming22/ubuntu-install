3b838b394d7a6d6aae248a72a2db9c9402831a3c
birdming22 <birdming22@hotmail.com>
2023-01-05 23:51:58 +0800

    Update fix-asbru.sh

--- 20.04/fix-asbru.sh
+++ 20.04/fix-asbru.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt-add-repository -y multiverse
-sudo apt install curl
+sudo apt install -y curl
 curl -1sLf 'https://dl.cloudsmith.io/public/asbru-cm/release/cfg/setup/bash.deb.sh' | sudo -E bash
-sudo apt install asbru-cm
-sudo apt install libglib2.0-dev cpanminus
+sudo apt install -y asbru-cm
+sudo apt install -y libglib2.0-dev cpanminus
 sudo cpanm -i Glib
